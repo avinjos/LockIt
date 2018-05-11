542e73f0e6ba59195c4085d6cb5c63590b12e077
avinjos <s.avinashjoshi@gmail.com>
2018-05-10 21:26:06 -0500

    Added APIs categories,category/id

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
-from flask import Flask ,flash,redirect,render_template,session,request,abort
+from flask import Flask, flash, redirect, render_template, session, request, abort, jsonify
 import os
-
 
 app = Flask(__name__)
 
+categories = [
+    {
+        "id": 1,
+        "name": "Application Accounts",
+        "Example": "Email,Subsriptions etc"
+    }
+    , {
+        "id": 2,
+        "name": "Bank Accounts",
+        "Example": "Credit Cards, Savings, Loan Accounts etc"
+    }, {
+        "id": 3,
+        "name": "Miscellaneous",
+        "Example": "Tracking Number, Application confirmation # etc"
+    }
+
+]
+
+
 @app.route("/")
 def home():
-       if not session.get('logged_in'):
-           return render_template('login.html')
-       else:
-           return "Hello Boss !"
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return categories()
 
-@app.route('/login',methods=['POST'])
+
+@app.route('/login', methods=['POST'])
 def do_admin_login():
     if request.form['password'] == 'password' and request.form['username'] == 'admin':
         session['logged_in'] = True
@@ -19,10 +38,26 @@ def do_admin_login():
         flash('Wrong Password !')
     return home()
 
+
+@app.route("/categories", methods=['GET'])
+def catogories():
+    return jsonify(categories)
+
+
+@app.route('/category/<int:key>')
+def category(key):
+    for category in categories:
+        if category['id'] == key:
+            return jsonify(category)
+
+    return abort(404)
+
+
 @app.route("/logout")
 def logout():
-     session['logged_in'] = False
-     return home()
+    session['logged_in'] = False
+    return home()
+
 
 app.secret_key = os.urandom(12)
 app.run()
